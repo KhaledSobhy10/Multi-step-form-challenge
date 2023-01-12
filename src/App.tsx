dc43cb6d1358b010daef838f3402817c636aed56
KhaledSobhy22 <khaledloda696@gmail.com>
2023-01-12 15:38:21 +0200

    add fonts and images

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-pink-500">Hello</div>
+  function testG<T>(s: T) {
+
+  }
+  return (
+    <div className="w-screen h-screen bg-black text-white p-2 ">
+      <h1 className="font-UbuntuRegular">Regular</h1>
+      <h1 className="font-UbuntuMedium">Medium</h1>
+      <h1 className="font-UbuntuBold bg-light-gray">Bold</h1>
+    </div>);
 }
 
 export default App;
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      UbuntuRegular: ["UbuntuRegular", "serif"],
+      UbuntuBold: ["UbuntuBold", "serif"],
+      UbuntuMedium: ["UbuntuMedium", "serif"],
+    },
+    colors: {
+      "marine-blue": "#02295A",
+      "purplish-blue": "#473DFF",
+      "pastel-blue": "#ADBEFF",
+      "light-blue": "#BFE2FD",
+      "strawberry-red": "#ED3548",
+      "cool-gray": "#9699AB",
+      "light-gray": "#D6D9E6",
+      magnolia: "#F0F6FF",
+      alabaster: "#FAFBFF",
+      white: "#FFFFFF",
+    },
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [],
+};
